15ce4c99351de0f919e9146b9da9221fe9243d3a
mits0s2019 <mits0s200efta@gmail.com>
2020-01-26 00:07:17 +0200

    movies-ui navbar thymeleaf 1

--- movies-client/src/main/java/com/example/moviesclient/controller/WelcomeController.java
+++ movies-client/src/main/java/com/example/moviesclient/controller/WelcomeController.java
@@ -1,14 +1,10 @@
 package com.example.moviesclient.controller;
 
-
-import com.example.moviesclient.config.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-import java.security.Principal;
 
 
 @Controller
@@ -19,10 +15,11 @@ public class WelcomeController {
     RestTemplate restTemplate;
 
     @RequestMapping(value = "/")
-    public String loadUI(Principal principal){
-//        System.out.println(AccessToken.getAccessToken());
-
-
+    public String loadUI(){
         return "index";
+    }
+    @RequestMapping(value = "/register")
+    public String register(){
+        return "register";
     }
 }
