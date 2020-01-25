a1b4fc82e4728318c4204fc97a2ad9ae6a051ede
mits0s2019 <mits0s200efta@gmail.com>
2020-01-25 23:10:12 +0200

    movies-ui principal 2

--- movies-client/src/main/java/com/example/moviesclient/controller/WelcomeController.java
+++ movies-client/src/main/java/com/example/moviesclient/controller/WelcomeController.java
@@ -3,22 +3,25 @@ package com.example.moviesclient.controller;
 
 import com.example.moviesclient.config.AccessToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.security.Principal;
 
 
 @Controller
 public class WelcomeController {
 
-//    @Autowired
-//    PrincipalExtractor principalExtractor;
+    @Autowired
+    PrincipalExtractor principalExtractor;
     @Autowired
     RestTemplate restTemplate;
 
     @RequestMapping(value = "/")
-    public String loadUI(){
-
+    public String loadUI(Principal principal){
+        System.out.println(principalExtractor.extractPrincipal());
         System.out.println(AccessToken.getAccessToken());
 
 
