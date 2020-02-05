13cc84984a6b1f7a53c60a496e18425c4ff2abf4
mits0s2019 <mits0s200efta@gmail.com>
2020-02-05 21:07:33 +0200

    after-search-page(2)

--- movies-client/src/main/resources/static/js/page_after_search.js
+++ movies-client/src/main/resources/static/js/page_after_search.js
@@ -37,7 +37,83 @@ App.controller("MainCtrl", function ($scope, $http) {
         }
     }
 
+
+
+
+    $scope.MyListFunction=function() {
+
+        if (accessToken !== "" && mediaType==="movie") {
+            let movieTitle = document.getElementById('movie-title').innerHTML;
+            let movieOverview = document.getElementById('movie-overview').innerHTML;
+            let moviePoster = document.getElementById('movie-poster').src;
+
+            let req = {
+                method: 'POST',
+                url: 'http://localhost:8090/movie/add',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': accessToken
+                },
+                data: {movieId: id, title: movieTitle, img: moviePoster, vote: null, description: movieOverview}
+            };
+            $http(req).then(function (data) {
+                    console.log(data.data)
+                    movieAdded(id);
+                },
+                function (error) {
+                    console.log(error)
+                });
+        }else if(accessToken !== "" && mediaType==="tv") {
+
+            let tvTitle = document.getElementById('tv-title').innerHTML;
+            let tvOverview = document.getElementById('tv-overview').innerHTML;
+            let tvPoster = document.getElementById('tv-poster').src;
+
+            let req = {
+                method: 'POST',
+                url: 'http://localhost:8090/tv-series/add',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': accessToken
+                },
+                data: {tvId: id, title: tvTitle, img: tvPoster, vote: null, description: tvOverview}
+            };
+            $http(req).then(function (data) {
+                    console.log(data.data)
+                    movieAdded(id);
+                },
+                function (error) {
+                    console.log(error)
+                });
+
+        }else{alert("You have to be logged in")}
+    }
+
     });
 });
 
 
+function movieAdded(id){
+
+    let button_id=document.getElementById('button-id');
+    let btnObj=new Button(id);
+            if(btnObj.clicked===false){
+                button_id.innerText="Added to List";
+                button_id.className='glyphicon glyphicon-pushpin btn input-block-level btn-success w-100';
+                btnObj.clicked=true;
+            }else{
+                button_id.innerText="Add to List";
+                button_id.className='glyphicon glyphicon-pushpin btn input-block-level btn-danger w-100';
+                btnObj.clicked=false;
+            }
+
+
+
+
+}
+
+
+let Button = function(id) {
+    this.id = id;
+    this.clicked=false;
+};
